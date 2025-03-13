26e41f7ce5b07f809a0dd1f7d6776e17d4a63c9b
Charindu Bandaranayake <charindu.cdb@gmail.com>
2025-03-13 15:08:00 +0530

    Add route to get total songs, albums, users, and unique artists

--- backend/src/routes/stats.route.js
+++ backend/src/routes/stats.route.js
@@ -1,8 +1,41 @@
-import { Router } from 'express';
-import { getStats } from '../controller/stats.controller.js';
+import { Router } from "express";
+import {Song} from "../models/song.model.js";
+import {Album} from "../models/album.model.js";
+import {User} from "../models/user.model.js";
+
 
 const router = Router();
 
-router.get("/", getStats);
+router.get("/", async (req, res, next) =>  {
+    try {
+        //const totalSongs = await Song.countDocument();
+        //const totalUsers = await User.countDocument();
+        //const totalAlbums = await Album.countDocument();
 
-export default router;
+        const [totalSongs, totalAlbums, totalUsers, uniqueArtists] = await Promise.all([
+            Song.countDocument(),
+            Album.countDocument(),
+            User.countDocument(),
+
+            Song.aggregate([
+                {
+                    $unionwidth: {
+                        colls: "Albums",
+                        pipeline: [],
+                    }
+                },
+                {
+                    $group: {
+                        _id: "$artist",
+                    }
+                },
+                {
+                    $count: "count,"
+                },
+            ]),
+     ]);
+    } catch (error) {
+        
+    }
+
+});
