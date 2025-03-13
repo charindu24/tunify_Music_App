cb5d40260cadcb74afd1db2bf7e8e95b1283aabd
Charindu Bandaranayake <charindu.cdb@gmail.com>
2025-03-13 15:06:07 +0530

    Add functions to get all songs, load featured, made for you, and trending songs

--- backend/src/controller/song.controller.js
+++ backend/src/controller/song.controller.js
@@ -1,3 +1,94 @@
-export const getSong = (req, res) => {
-    res.send("Song route with get method!");
+import { Song } from "../models/song.model.js";
+
+export const getAllSongs = async (req, res) => {
+    try {
+        const songs = await Song.find().sort({createdAt: -1});
+        // Find all songs in the database and sort them by creation date (newest first)
+// Explanation:
+// - The `find()` method retrieves all songs from the database.
+// - `sort({ createdAt: -1 })` sorts them in descending order.
+// - `-1` means "newest first" (latest `createdAt` timestamp).
+// - If we used `1` instead, it would sort in ascending order (oldest first).
+res.json(songs);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const loadFeaturedSongs = async (req, res) => {
+    try {
+    // Perform an aggregation pipeline operation on the Song collection
+        const songs = await Song.aggregate([
+            {
+                $sample:{ size: 6 },
+            },
+            {
+                $project:{
+                    _id:1,
+                    title:1,
+                    artist:1,
+                    imageUrl:1,
+                    audioUrl:1,
+                },
+            },
+        ]);
+
+        res.json(songs);
+
+    } catch (error) {
+        next(error);
+        
+    };
+};
+
+export const loadMadeForYouSongs = async (req, res) => {
+    try {
+        // fet
+            const songs = await Song.aggregate([
+                {
+                    $sample:{ size: 4 },
+                },
+                {
+                    $project:{
+                        _id:1,
+                        title:1,
+                        artist:1,
+                        imageUrl:1,
+                        audioUrl:1,
+                    },
+                },
+            ]);
+    
+            res.json(songs);
+    
+        } catch (error) {
+            next(error);
+            
+        };
+};
+
+export const loadTrendingSongs = async (req, res) => {
+    try {
+        // Perform an aggregation pipeline operation on the Song collection
+            const songs = await Song.aggregate([
+                {
+                    $sample:{ size: 4 },
+                },
+                {
+                    $project:{
+                        _id:1,
+                        title:1,
+                        artist:1,
+                        imageUrl:1,
+                        audioUrl:1,
+                    },
+                },
+            ]);
+    
+            res.json(songs);
+    
+        } catch (error) {
+            next(error);
+            
+        };
 };
