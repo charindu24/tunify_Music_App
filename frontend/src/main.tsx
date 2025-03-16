ac37e7a111ae90c46aa7dc81cb5a3ca6cde2c3bd
Charindu Bandaranayake <charindu.cdb@gmail.com>
2025-03-16 15:33:43 +0530

    Wrap app with BrowserRouter and AuthProvider for routing and authentication

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import './index.css'
+import App from './App.tsx'
 import { ClerkProvider } from '@clerk/clerk-react'
+import { BrowserRouter } from 'react-router-dom'
+import AuthProvider from './providers/AuthProvider.tsx'
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -11,10 +14,15 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
 }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <App />
+    <AuthProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
     </ClerkProvider>
-  </React.StrictMode>,
-)
+  </StrictMode>,
+);
+
