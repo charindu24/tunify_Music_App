7c6f4ff18335e20c5e3996f7cfdd32ab483e745f
Charindu Bandaranayake <charindu.cdb@gmail.com>
2025-03-16 13:08:55 +0530

    Add stats route with admin protection

--- backend/src/routes/stats.route.js
+++ backend/src/routes/stats.route.js
@@ -1,41 +1,11 @@
 import { Router } from "express";
-import {Song} from "../models/song.model.js";
-import {Album} from "../models/album.model.js";
-import {User} from "../models/user.model.js";
+
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
+import { getStats } from "../controller/stats.controller.js";
 
 
 const router = Router();
 
-router.get("/", async (req, res, next) =>  {
-    try {
-        //const totalSongs = await Song.countDocument();
-        //const totalUsers = await User.countDocument();
-        //const totalAlbums = await Album.countDocument();
+router.get("/", protectRoute, requireAdmin, getStats);
 
-        const [totalSongs, totalAlbums, totalUsers, uniqueArtists] = await Promise.all([
-            Song.countDocument(),
-            Album.countDocument(),
-            User.countDocument(),
-
-            Song.aggregate([
-                {
-                    $unionwidth: {
-                        colls: "Albums",
-                        pipeline: [],
-                    }
-                },
-                {
-                    $group: {
-                        _id: "$artist",
-                    }
-                },
-                {
-                    $count: "count,"
-                },
-            ]),
-     ]);
-    } catch (error) {
-        
-    }
-
-});
+export default router;
