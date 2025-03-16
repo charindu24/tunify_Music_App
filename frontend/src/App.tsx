e0bde4385be652530c5decdfd74378564a78e276
Charindu Bandaranayake <charindu.cdb@gmail.com>
2025-03-16 15:33:01 +0530

    Fix API call with authorization token and routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,32 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import AuthCallbackPage from "./pages/authCallback/AuthCallbackPage";
+import { axiosInstance } from "./lib/axios";
+
 
 export default function App() {
+
+  //token =>
+
+  const getSomeData = async() => {
+    const res = await axiosInstance.get("/users", {
+      headers: {
+        "Authorization": `Bearer ${token}`
+      }
+    });
+    console.log(res);
+  }
   return (
-    <header>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
-  );
+    <>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/auth-callback" element={<AuthCallbackPage />} />
+
+
+
+    </Routes>
+    
+    </>
+  )
 }
